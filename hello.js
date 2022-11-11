33a69833b89bdc1f671f540a3498a80a9d8f059c
Okada <ringoapple.sukebasuke@gmail.com>
2022-11-11 19:35:58 +0900

    平方根の関数を定義

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
-console.log("hello");
+console.log("hello world");
+
+let points = [
+  {x: 0, y: 0},
+  {x: 1, y: 1}
+];
+
+points.dist = function() {
+  let p1 = this[0];
+  let p2 = this[1];
+  let a = p2.x - p1.x;
+  let b = p2.y - p1.y;
+  return Math.sqrt(a*a + b*b);
+};
