bf2d6644e9cb662d95093c6723ff8b4d9a8513a4
Okada <ringoapple.sukebasuke@gmail.com>
2022-11-11 20:08:09 +0900

    配列をたす関数sumを定義

--- hello.js
+++ hello.js
@@ -5,6 +5,8 @@ let points = [
   {x: 1, y: 1}
 ];
 
+let primes = [2, 3, 5, 7];
+
 points.dist = function() {
   let p1 = this[0];
   let p2 = this[1];
@@ -12,3 +14,20 @@ points.dist = function() {
   let b = p2.y - p1.y;
   return Math.sqrt(a*a + b*b);
 };
+
+function ads(x) {
+  if (x >= 0) {
+    return x;
+  } 
+  else {
+    return -x;
+  }
+}
+
+function sum(array) {
+  let sum = 0;
+  for(let x of array) {
+    sum += x;
+  }
+  return sum;
+}
